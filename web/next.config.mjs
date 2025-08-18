b9e91e599bd86d1439a86fa1aab783f322cc6ea6
Charlie Klein <gatedf355@users.noreply.github.com>
2025-08-18 08:25:31 -0400

    web: add webpack alias '@' -> web/

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,5 +1,10 @@
+import path from 'node:path'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname)
+    return config
+  },
   async rewrites() {
     if (process.env.NODE_ENV === 'development') {
       return [{ source: '/api/:path*', destination: 'http://localhost:4000/api/:path*' }]
